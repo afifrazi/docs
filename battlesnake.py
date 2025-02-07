339cba767c8b5b31ae7fd17489130a24fef691c6
afifrazi <afifrazi26400@gmail.com>
2025-02-08 04:01:43 +0800

    Create battlesnake.py

---
+++ battlesnake.py
@@ -0,0 +1,20 @@
+{
+  "game": {"id": "game-id"},
+  "turn": 5,
+  "board": {
+    "height": 11,
+    "width": 11,
+    "snakes": [
+      {
+        "id": "your-snake-id",
+        "name": "MyBattlesnake",
+        "health": 90,
+        "body": [{"x": 5, "y": 5}, {"x": 5, "y": 4}, {"x": 5, "y": 3}]
+      }
+    ]
+  },
+  "you": {
+    "id": "your-snake-id",
+    "body": [{"x": 5, "y": 5}, {"x": 5, "y": 4}, {"x": 5, "y": 3}]
+  }
+}
